3bc6a681e845019c5a99cfc249755a404a4acbc4
KarthyV <Kdon979@gmail.com>
2022-11-17 09:55:36 +0530

    previewCard - time added

--- src/RecipePreview.jsx
+++ src/RecipePreview.jsx
@@ -3,16 +3,19 @@ import React from "react";
 
 const RecipePreview = ({ recipe }) => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ minWidth: 300, minHeight: 200 }}>
       <CardMedia
         component="img"
         height="140"
         image={recipe.recipePoster}
         alt={recipe.recipeName}
       />
-      <CardContent>
+      <CardContent className="preview_content">
         <Typography gutterBottom variant="h5" component="div">
           {recipe.recipeName}
+        </Typography>
+        <Typography className="cookingTime" variant="span" component="span">
+          🕒{recipe.cookingTime}m
         </Typography>
       </CardContent>
     </Card>
